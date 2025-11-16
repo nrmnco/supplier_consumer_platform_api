6b9a19ffc73aabc10e5712ecb8d59870043da5ec
nrmnco <issayevnariman@gmail.com>
2025-11-16 17:32:13 +0500

    add get /users

--- src/cruds/user.py
+++ src/cruds/user.py
@@ -9,3 +9,6 @@ def get_user_by_id(session: Session, user_id: str) -> Users | None:
 
 def get_user_by_phone(session: Session, phone_number: str):
     return session.exec(select(Users).where(Users.phone_number == phone_number)).first()
+
+def get_all_users(session: Session, company_id: int):
+    return session.exec(select(Users).where(Users.company_id == company_id)).all()
--- src/routes/user.py
+++ src/routes/user.py
@@ -3,7 +3,8 @@ from sqlmodel import Session
 
 from src.core.database import get_session
 from src.core.security import check_access_token
-from src.cruds.user import get_user_by_email, get_user_by_id
+from src.cruds.user import get_user_by_email, get_user_by_id, get_all_users
+from src.models.users import UserRole
 
 
 router = APIRouter(prefix="/user", tags=["User"])
@@ -27,3 +28,20 @@ async def read_all_users(user_id: int, user: str = Depends(check_access_token), 
         raise HTTPException(status_code=404, detail="User not found")
     
     return user
+
+
+@router.get("/")
+async def all_users(user: str = Depends(check_access_token), session: Session = Depends(get_session)):
+    email = user.get("sub")
+    
+    user = get_user_by_email(session, email)
+
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    if user.role not in (UserRole.owner, UserRole.manager):
+        raise HTTPException(status_code=403, detail="Insufficient permissions to view all users")
+    
+    users = get_all_users(session, user.company_id)
+    
+    return {"users": users}
