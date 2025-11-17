121024adffaabbcf11914e2d047d423bee54f310
nrmnco <issayevnariman@gmail.com>
2025-11-18 02:53:33 +0500

    added get all companies

--- src/cruds/company.py
+++ src/cruds/company.py
@@ -3,3 +3,6 @@ from src.models.companies import Companies
 
 def get_company_by_id(session: Session, id: int) -> Companies | None:
     return session.exec(select(Companies).where(Companies.company_id == id)).first()
+
+def get_all_companies(session: Session): 
+    return session.exec(select(Companies).where(Companies.company_type == "supplier")).all()
--- src/routes/company.py
+++ src/routes/company.py
@@ -3,7 +3,8 @@ from sqlmodel import Session
 
 from src.core.database import get_session
 from src.core.security import check_access_token
-from src.cruds.company import get_company_by_id
+from src.cruds.company import get_company_by_id, get_all_companies
+from src.cruds.user import get_user_by_email
 
 router = APIRouter(prefix="/company", tags=["Company"])
 
@@ -16,3 +17,15 @@ async def get_company(company_id: int, user: str = Depends(check_access_token), 
     
     return company
 
+
+@router.get("/")
+async def get_companis(user: str = Depends(check_access_token), session: Session = Depends(get_session)):
+    user = get_user_by_email(session, user['sub'])
+    company = get_company_by_id(session, user.company_id)
+
+    if company.company_type != "consumer":
+        return HTTPException(status_code=403, detail="Not enough rights")
+    
+    companies = get_all_companies(session)
+
+    return {"companies": companies}
