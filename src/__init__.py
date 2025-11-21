71254182c9b4b0f97f306b9852112e0836ff5e05
nrmnco <issayevnariman@gmail.com>
2025-11-22 02:20:57 +0500

    fix

--- src/__init__.py
+++ src/__init__.py
@@ -59,7 +59,6 @@ app.include_router(router)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "*",
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "http://localhost:8000",
